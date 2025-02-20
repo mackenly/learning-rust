c2b9d3e9e0a9bf31370986d94ecc1d1026d5c9fc
Mackenly Jones <53151058+mackenly@users.noreply.github.com>
2025-02-20 12:58:57 -0500

    Fix lost crypto drive

--- projects/lost_crypto_drive/src/main.rs
+++ projects/lost_crypto_drive/src/main.rs
@@ -1,13 +1,18 @@
 use chrono::{Duration, Local};
 use serde_json::{Result, Value};
 
+// Notice: The API I was using seems to have became more restrictive
+// Ex: It doesn't allow historical data for over a year ago
+// And it seems highliy rate limited
+// So this might not work and needs to be updated to work with a different API
+
 #[tokio::main]
 async fn main() {
-    println!("We've all heard it before, \"If I only knew where I put that hard drive! I had to have had hundreds of bitcoins on it!\" Well, now you can see how much you allegedly lost with this tool which shows how much an investment five years ago would be worth today.");
+    println!("We've all heard it before, \"If I only knew where I put that hard drive! I had to have had hundreds of bitcoins on it!\" Well, now you can see how much you allegedly lost with this tool which shows how much an investment a year ago would be worth today.");
     // do while
     loop {
         let investment_amount: f64 = get_investment_amount();
-        let five_years_ago: String = get_date_from_five_years_ago();
+        let a_year_ago: String = get_date_from_a_year_ago();
         let today: String = Local::now().format("%d-%m-%Y").to_string();
         let tickers: Vec<String> = vec![
             "bitcoin".to_string(),
@@ -16,17 +21,17 @@ async fn main() {
         ];
 
         for ticker in tickers {
-            let price_five_years_ago: f64 = get_price(ticker.clone(), five_years_ago.clone())
+            let price_a_year_ago: f64 = get_price(ticker.clone(), a_year_ago.clone())
                 .await
                 .unwrap();
             let price_today: f64 = get_price(ticker.clone(), today.clone()).await.unwrap();
-            let investment_amount_in_crypto_five_years_ago: f64 =
-                investment_amount / price_five_years_ago;
+            let investment_amount_in_crypto_a_year_ago: f64 =
+                investment_amount / price_a_year_ago;
             let value_of_investment_in_crypto_today: f64 =
-                investment_amount_in_crypto_five_years_ago * price_today;
+                investment_amount_in_crypto_a_year_ago * price_today;
             let difference: f64 = value_of_investment_in_crypto_today
-                - investment_amount_in_crypto_five_years_ago * price_five_years_ago;
-            println!("If you invested ${:.2} in {} five years ago, you would have {:.2} {} worth ${:.2} today. That's a difference of ${:.2} dollars.", investment_amount, ticker, investment_amount_in_crypto_five_years_ago, ticker, value_of_investment_in_crypto_today, difference);
+                - investment_amount_in_crypto_a_year_ago * price_a_year_ago;
+            println!("If you invested ${:.2} in {} five years ago, you would have {:.2} {} worth ${:.2} today. That's a difference of ${:.2} dollars.", investment_amount, ticker, investment_amount_in_crypto_a_year_ago, ticker, value_of_investment_in_crypto_today, difference);
         }
 
         let input: String = prompt_for_input("\nWould you like to go again? (y/n)");
@@ -52,14 +57,14 @@ fn get_investment_amount() -> f64 {
     investment_amount
 }
 
-fn get_date_from_five_years_ago() -> String {
+fn get_date_from_a_year_ago() -> String {
     // get today's date
     let now: chrono::DateTime<Local> = Local::now();
     // subtract five years from today's date
-    let five_years_ago: chrono::DateTime<Local> = now - Duration::days(1825);
+    let a_year_ago: chrono::DateTime<Local> = now - Duration::days(364);
     // format the date to be in the format of the API dd-mm-yyyy
-    let five_years_ago: String = five_years_ago.format("%d-%m-%Y").to_string();
-    five_years_ago
+    let a_year_ago: String = a_year_ago.format("%d-%m-%Y").to_string();
+    a_year_ago
 }
 
 async fn http_request(url: &str) -> String {
@@ -94,6 +99,7 @@ async fn get_price(ticker: String, date: String) -> Result<f64> {
             .as_f64()
             .unwrap()
     };
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     Ok(price)
 }
 
@@ -114,32 +120,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_date_from_five_years_ago() {
+    fn test_get_date_from_a_year_ago() {
         let now: chrono::DateTime<Local> = Local::now();
-        let five_years_ago: chrono::DateTime<Local> = now - Duration::days(1825);
-        let five_years_ago: String = five_years_ago.format("%d-%m-%Y").to_string();
-        assert_eq!(get_date_from_five_years_ago(), five_years_ago);
+        let a_year_ago: chrono::DateTime<Local> = now - Duration::days(364);
+        let a_year_ago: String = a_year_ago.format("%d-%m-%Y").to_string();
+        assert_eq!(get_date_from_a_year_ago(), a_year_ago);
     }
 
     #[tokio::test]
     async fn test_get_price() {
         assert_eq!(
-            get_price("bitcoin".to_string(), "18-11-2023".to_string())
+            get_price("bitcoin".to_string(), "18-11-2024".to_string())
                 .await
                 .unwrap(),
-            36527.76022530742
+            89841.47194135105
         );
         assert_eq!(
-            get_price("ethereum".to_string(), "18-11-2023".to_string())
+            get_price("ethereum".to_string(), "18-11-2024".to_string())
                 .await
                 .unwrap(),
-            1956.5174321166123
+            3075.534532601624
         );
         assert_eq!(
-            get_price("dogecoin".to_string(), "18-11-2023".to_string())
+            get_price("dogecoin".to_string(), "18-11-2024".to_string())
                 .await
                 .unwrap(),
-            0.08602320148882048
+            0.36707674107673377
         );
     }
 }
